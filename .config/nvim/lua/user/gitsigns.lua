279d482bfa0725a726a8cd998f944e0846e6c05b
ItsMeSamey <sameychain5041@gmail.com>
2024-10-12 01:33:11 +0530

    updated ditSigns

--- .config/nvim/lua/user/gitsigns.lua
+++ .config/nvim/lua/user/gitsigns.lua
@@ -91,7 +91,6 @@ require('gitsigns').setup({
     map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
     map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
     map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
-    -- map('n', '<leader>hU', gs.undo_stage_buffer, { desc = 'git Stage buffer' })
     map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
     map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
     map('n', '<leader>hb', function() gs.blame_line { full = false } end, { desc = 'git blame line' })
