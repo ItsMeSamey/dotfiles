a76c2a168b34ab11539b66671e827703ad56baa7
ItsMeSamey <sameychain5041@gmail.com>
2024-10-12 01:27:56 +0530

    gitdiff

--- .config/nvim/lua/user/gitsigns.lua
+++ .config/nvim/lua/user/gitsigns.lua
@@ -1,12 +1,53 @@
 require('gitsigns').setup({
   -- See `:help gitsigns.txt`
   signs = {
-    add = { text = '+' },
-    change = { text = '~' },
-    delete = { text = '_' },
-    topdelete = { text = '‾' },
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
     changedelete = { text = '~' },
+    untracked    = { text = '┆' },
   },
+  signs_staged = {
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+  signs_staged_enable = true,
+  signcolumn = true,
+  numhl      = false,
+  linehl     = false,
+  word_diff  = false,
+  watch_gitdir = {
+    follow_files = true
+  },
+  auto_attach = true,
+  attach_to_untracked = false,
+  current_line_blame = true,
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
+    delay = 0,
+    ignore_whitespace = false,
+    virt_text_priority = 100,
+    use_focus = true,
+  },
+  sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  max_file_length = 1000 * 1000,
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = 'single',
+    style = 'minimal',
+    relative = 'cursor',
+    row = 0,
+    col = 1
+  },
+
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
 
@@ -47,18 +88,15 @@ require('gitsigns').setup({
     end, { desc = 'reset git hunk' })
     -- normal mode
     map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
+    map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
     map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
     map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
-    map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
+    -- map('n', '<leader>hU', gs.undo_stage_buffer, { desc = 'git Stage buffer' })
     map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
     map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
-    map('n', '<leader>hb', function()
-      gs.blame_line { full = false }
-    end, { desc = 'git blame line' })
+    map('n', '<leader>hb', function() gs.blame_line { full = false } end, { desc = 'git blame line' })
     map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
-    map('n', '<leader>hD', function()
-      gs.diffthis '~'
-    end, { desc = 'git diff against last commit' })
+    map('n', '<leader>hD', function() gs.diffthis '~' end, { desc = 'git diff against last commit' })
 
     -- Toggles
     map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
