4422277a10c63a3f8a99af7c0a4dde55460061f0
ItsMeSamey <sameychain5041@gmail.com>
2025-02-17 19:17:39 +0530

    update

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -123,6 +123,18 @@ require('lazy').setup({
     },
   },
 
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+      "neovim/nvim-lspconfig", -- optional
+    },
+    opts = {} -- your configuration
+  },
+
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = {
